de4e4716ef558df4b44191c88e1705b844dd1a3d
Szhshp <24031shp@sina.com>
2025-01-18 14:41:16 +0800

    refactor: update variable declarations and improve function syntax; rename package to wiznote2markdown

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
+#!/usr/bin/env node
 
-const sqlite3 = require("sqlite3").verbose();
-const unzipper = require('unzipper');
-const fs = require('fs');
-const TurndownService = require('turndown');
-const rimraf = require('rimraf');
+var sqlite3 = require("sqlite3").verbose();
+var unzipper = require('unzipper');
+var fs = require('fs');
+var TurndownService = require('turndown');
+var rimraf = require('rimraf');
 
 // Delete /export and /final directories before running anything
 rimraf.sync('export');
 rimraf.sync('final');
 
 // Open an existing database file
-const db = new sqlite3.Database('index.db', (err) => {
+var db = new sqlite3.Database('index.db', function (err) {
   if (err) {
     console.error('Error opening database:', err.message);
   } else {
@@ -21,49 +22,48 @@ const db = new sqlite3.Database('index.db', (err) => {
 /**
  * Show all rows in the WIZ_DOCUMENT table
  */
-db.all("SELECT * FROM WIZ_DOCUMENT", [], (err, rows) => {
+db.all("SELECT * FROM WIZ_DOCUMENT", [], function (err, rows) {
   if (err) {
     console.error('Error fetching rows:', err.message);
     return;
   }
-  rows.forEach((row) => {
-    const id = row.DOCUMENT_GUID;
-    const expectedDir = row.DOCUMENT_LOCATION;
-    const file = row.DOCUMENT_TITLE;
+  rows.forEach(function (row) {
+    var id = row.DOCUMENT_GUID;
+    var expectedDir = row.DOCUMENT_LOCATION;
+    var file = row.DOCUMENT_TITLE;
 
-    getContent({ id, expectedDir, file });
+    getContent({ id: id, expectedDir: expectedDir, file: file });
   });
 });
 
-const getContent = async ({ id, expectedDir, file }) => {
-  const originalWizNotesPath = `notes/{${id}}`;
-  const exportPath = `export/unzipped/${id}`;
-  const finalPath = `final${expectedDir}${file}`;
+var getContent = function (params) {
+  var id = params.id;
+  var expectedDir = params.expectedDir;
+  var file = params.file;
+  var originalWizNotesPath = 'notes/{' + id + '}';
+  var exportPath = 'export/unzipped/' + id;
+  var finalPath = 'final' + expectedDir + file;
 
-  [exportPath, finalPath].forEach((path) => {
+  [exportPath, finalPath].forEach(function (path) {
     if (!fs.existsSync(path)) {
       fs.mkdirSync(path, { recursive: true });
     }
   });
 
-  try {
-    // Extract the contents of the zip file and save them to the export directory
-    const directory = await unzipper.Open.file(originalWizNotesPath);
-    await directory.extract({ path: exportPath });
-
-    // Convert index.html to markdown
-    const htmlPath = `${exportPath}/index.html`;
-    const markdownPath = `${exportPath}/index.md`;
-    const html = fs.readFileSync(htmlPath, 'utf8');
-    const turndownService = new TurndownService();
-    const markdown = turndownService.turndown(html);
+  unzipper.Open.file(originalWizNotesPath).then(function (directory) {
+    return directory.extract({ path: exportPath });
+  }).then(function () {
+    var htmlPath = exportPath + '/index.html';
+    var markdownPath = exportPath + '/index.md';
+    var html = fs.readFileSync(htmlPath, 'utf8');
+    var turndownService = new TurndownService();
+    var markdown = turndownService.turndown(html);
     fs.writeFileSync(markdownPath, markdown);
     fs.unlinkSync(htmlPath);
 
-    // Move the folder to the expected directory
     fs.renameSync(exportPath, finalPath);
     console.log('Content processed:', finalPath);
-  } catch (err) {
+  }).catch(function (err) {
     console.error('Error processing content:', err.message);
-  }
+  });
 };
